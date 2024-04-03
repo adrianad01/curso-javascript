b39cbf3bd0009a1b5f04de93a472561786cdd6f0
Adrian Alcocer <jadrianaldo@outlook.com>
2024-04-02 19:30:09 -0600

    Update Repo Curso de JS: Ejercicios DOM-En Curso

---
+++ ejerciciosDom/index_dom.js
@@ -0,0 +1,5 @@
+import hamburguerMenu from "./menu_hamburguesa";
+const d = document;
+d.addEventListener("DOMContentLoaded", (e) => {
+  hamburguerMenu(".panel-btn", "panel");
+});
---
+++ ejerciciosDom/menu_hamburguesa.js
@@ -0,0 +1,9 @@
+export default function hamburguerMenu(panelBtn, panel) {
+  const d = document;
+
+  d.addEventListener("click", e => {
+    if(e.target.matches(panelBtn)){
+        d.querySelector(panel).classList.toggle("is-active")
+    }
+  });
+}
