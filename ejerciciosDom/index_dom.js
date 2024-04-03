b2ee64d2a768bfb07e997b9a2bb60b4731c61cff
Adrian Alcocer <jadrianaldo@outlook.com>
2024-04-02 20:01:58 -0600

    Update Repo Curso de JS: Ejercicios DOM-En Curso: Finalizado Menú Hamburguesa

--- ejerciciosDom/index_dom.js
+++ ejerciciosDom/index_dom.js
@@ -1,5 +1,11 @@
-import hamburguerMenu from "./menu_hamburguesa";
+// Importa el módulo "menu_hamburguesa.js" y lo asigna a la variable hamburguerMenu
+import hamburguerMenu from "./menu_hamburguesa.js";
+
+// Obtiene el documento actual y lo asigna a la variable d
 const d = document;
+
+// Añade un event listener que escucha el evento "DOMContentLoaded" del documento
 d.addEventListener("DOMContentLoaded", (e) => {
-  hamburguerMenu(".panel-btn", "panel");
+  // Llama a la función hamburguerMenu, pasando los selectores ".panel-btn", ".panel" y ".menu a" como argumentos
+  hamburguerMenu(".panel-btn", ".panel", ".menu a");
 });
